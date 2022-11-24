10efc1383a1ec44d1f53210de2a6c78185244eb3
Samuel Faria Garcia <samuel.faria2002@hotmail.com>
2022-11-24 14:55:52 -0300

    Terminada pagina de usuário

--- src/pages/UserPage/index.js
+++ src/pages/UserPage/index.js
@@ -1,4 +1,4 @@
-import { View, ScrollView, TouchableHighlight, TouchableOpacity, Image, Text } from 'react-native'
+import {Text, View, Image, TouchableOpacity, StyleSheet, ScrollView, SafeAreaView, StatusBar } from 'react-native'
 import React, { Component } from 'react'
 import styles from './style'
 import { useNavigation } from '@react-navigation/native'
@@ -8,20 +8,43 @@ export default function UserPage() {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
-        <View style={styles.HeaderBar}>
-          <TouchableOpacity style={{ width: '100%', flexDirection: "row", marginTop: 20, justifyContent: 'center' }}>
-            <Image style={{ height: 200, width: '50%', borderRadius: 100 }}
-              source={require('../../assets/PessoaFotoPerfil.png')}
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={{ width: '100%', marginTop: 20, alignItems: 'center' }}>
-          <Text style={{ fontSize: 40 }}>Leonardo da Silva</Text>
-        </View>
-        <View style={styles.card}>
-          <Text style={{ fontSize: 40 }}>Engenheiro Militar</Text>
-        </View>
-    </View>
+    <SafeAreaView style={styles.main}>
+        <StatusBar backgroundColor="#7bb7e0" barStyle="light-content"/>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={{justifyContent: 'center', alignItems: 'center'}}
+        >
+          <Image style={styles.imageUser} source={require('../../assets/user-8.jpg')}></Image>
+          <Text style={styles.userName}>Bianca Ferreira</Text>
+          <Text style={styles.aboutUser}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a elit nisl.</Text>
+          <View style={styles.userBtnWrapper}>
+            <TouchableOpacity style={styles.userBtn} onPress={() => {}}>
+              <Text style={styles.userBtnTxt}>Logout</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.userBtn} onPress={() => {}}>
+              <Text style={styles.userBtnTxt}>Alterar dados</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.userOrdersBox}>
+            <Text style={styles.userOrderTitle}>Pedidos Cadastrados</Text>
+          </View>
+          <View style={styles.userInfoWrapper}>
+            <View style={styles.userInfoItem}>
+              <Text style={styles.userInfoTitle}>16</Text>
+              <Text style={styles.userInfoSubTitle}>Realizados</Text>
+            </View>
+            <View style={styles.userInfoItem}>
+              <Text style={styles.userInfoTitle}>10</Text>
+              <Text style={styles.userInfoSubTitle}>Completos</Text>
+            </View>
+            <View style={styles.userInfoItem}>
+              <Text style={styles.userInfoTitle}>6</Text>
+              <Text style={styles.userInfoSubTitle}>Cancelados</Text>
+            </View>
+          </View>
+
+        </ScrollView>
+      </SafeAreaView>
   )
 }
--- src/pages/UserPage/style.js
+++ src/pages/UserPage/style.js
@@ -1,14 +1,80 @@
-import { StyleSheet, Dimensions } from "react-native"
+import { StyleSheet } from 'react-native';
 
 exports.styles = StyleSheet.create({
-    HeaderBar: {
-        padding: 10,
+
+    main: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 20
+    }, 
+    imageUser: {
+        height: 150,
+        width: 150,
+        borderRadius: 75
+    },
+    userName: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        marginTop: 10,
+        marginBottom: 10,
+        color: '#000'
+    },
+    aboutUser: {
+        fontSize: 17,
+        fontWeight: '600',
+        color: '#666',
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    userBtnWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'center',
         width: '100%',
-        backgroundColor: '#7bb7e0'
+        marginBottom: 10,
     },
-    card: {
-        height: Dimensions.get("window").height,
-        backgroundColor: 'blue',
-        margin: 200
+    userBtn: {
+        borderColor: '#7bb7e0',
+        borderWidth: 2,
+        borderRadius: 3,
+        paddingVertical: 8,
+        paddingHorizontal: 12,
+        marginHorizontal: 5,
     },
-})
+    userBtnTxt: {
+        color: '#2e64e5',
+    },
+    userOrdersBox: {
+        justifyContent: 'center'
+    },
+    userOrderTitle: {
+        fontSize: 25,
+        marginTop: 40,
+        color: '#000'
+    },
+    userInfoWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '100%',
+        marginVertical: 20,
+    },
+    userInfoItem: {
+        justifyContent: 'center',
+    },
+    userInfoTitle: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginBottom: 5,
+        textAlign: 'center',
+        color: '#000'
+    },
+    userInfoSubTitle: {
+        fontSize: 16,
+        color: '#666',
+        textAlign: 'center',
+    },
+});
+
